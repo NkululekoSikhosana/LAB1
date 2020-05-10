2d0689dc5cbb1420454bdf1d81cfd542e8efb866
NkululekoSikhosana <1135124@students.wits.ac.za>
2020-05-10 20:56:02 +0200

    Adding LAB2 folder to repo

---
+++ ExpressFolder/index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// const path = require('path')
+const express = require('express')
+const app = express()
+const mainRouter = require('./mainRoutes')
+
+// app.get('/', function (req, res) {
+//  res.send('Hello World!!')
+// })
+
+// app.get('/about', (req, res) =>{
+//  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
+
+// mainRouter.get('/', function (req, res) {
+//  res.send('Hello World!!')
+// })
+
+// mainRouter.get('/about', (req, res) => {
+//  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+// })
+
+app.use(mainRouter)
+
+app.listen(3000)
+console.log('Express server running on port 3000')
---
+++ ExpressFolder/mainRoutes.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const path = require('path')
+const express = require('express')
+const mainRouter = express.Router()
+mainRouter.get('/', function (req, res) {
+  res.send('Hello World!!')
+})
+
+mainRouter.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+module.exports = mainRouter
