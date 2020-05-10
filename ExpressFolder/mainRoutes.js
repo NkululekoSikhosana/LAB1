dbbc6f53003cf0ccc62c47e425d4a5cd434b2049
NkululekoSikhosana <1135124@students.wits.ac.za>
2020-05-10 21:05:29 +0200

    Ports and ruoted message update

--- ExpressFolder/mainRoutes.js
+++ ExpressFolder/mainRoutes.js
@@ -4,7 +4,7 @@ const path = require('path')
 const express = require('express')
 const mainRouter = express.Router()
 mainRouter.get('/', function (req, res) {
-  res.send('Hello World!!')
+  res.send('Hello World!!, I\'m Node.js')
 })
 
 mainRouter.get('/about', (req, res) => {
